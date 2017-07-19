a3184ce76168b66daa02eaa28d70b25c56096b80
Matt Coffin <matt.coffin@charter.com>
2017-07-19 15:49:43 -0600

    Allow exclusions

--- src/Harbaror/Main.purs
+++ src/Harbaror/Main.purs
@@ -6,15 +6,17 @@ import Control.Monad.Eff.Exception (EXCEPTION, Error, error)
 import Control.Monad.Eff.Unsafe (unsafeCoerceEff)
 import Control.Monad.Error.Class (class MonadError, throwError)
 import Control.Monad.ST (newSTRef, readSTRef, modifySTRef)
-import Data.Foldable (fold)
+import Data.Foldable (fold, or)
 import Data.Foreign (Foreign)
 import Data.Int as I
 import Data.Maybe (Maybe(..), fromMaybe, maybe)
 import Data.Newtype (unwrap)
 import Data.Nullable (Nullable, toMaybe)
 import Data.Options (Options, (:=))
+import Data.String.Regex (Regex)
+import Data.StrMap as SM
 import Data.Tuple (Tuple(..))
-import Harbaror.Config (Webhook, webhookRequestHeaders)
+import Harbaror.Config (Webhook, webhookRequestHeaders, webhookExclusions)
 import Mustache (render)
 import Node.Encoding as Encoding
 import Node.HTTP (HTTP)
@@ -29,23 +31,30 @@ defaultPort :: String -> Int
 defaultPort "http" = 80
 defaultPort _ = 443
 
+foreign import testExclusion :: String -> Regex -> Foreign -> Boolean
+
 doRequest :: ∀ ε. Webhook -> Foreign -> Aff (exception :: EXCEPTION, http :: HTTP | ε) String
 doRequest webhook view = do
     Tuple requestOptions requestBody <- makeRequest webhook view
-    res <- makeAff \onE onS -> do
-        req <- HTTPC.request requestOptions onS
-        let reqStream = requestAsStream req
-        S.onError reqStream onE
-        void $ maybe (S.end reqStream (pure unit)) (\rb -> void (S.writeString reqStream Encoding.UTF8 rb $ S.end reqStream (pure unit))) requestBody
-    let resStream = responseAsStream res
-    makeAff \onE onS -> do
-        resBody <- unsafeCoerceEff $ newSTRef ""
-        S.onError resStream onE
-        S.onEnd resStream do
-            b <- unsafeCoerceEff $ readSTRef resBody
-            onS b
-        S.onDataString resStream Encoding.UTF8 \s -> unsafeCoerceEff $ void $ modifySTRef resBody (flip (<>) s)
-        pure unit
+    exclusions <- (SM.toUnfoldable <$> webhookExclusions webhook) :: _ (Array (Tuple String Regex))
+    let shouldExclude = or $ exclusions <#> \(Tuple query pattern) -> testExclusion query pattern view
+    if shouldExclude
+       then pure "Excluded"
+       else do
+           res <- makeAff \onE onS -> do
+               req <- HTTPC.request requestOptions onS
+               let reqStream = requestAsStream req
+               S.onError reqStream onE
+               void $ maybe (S.end reqStream (pure unit)) (\rb -> void (S.writeString reqStream Encoding.UTF8 rb $ S.end reqStream (pure unit))) requestBody
+           let resStream = responseAsStream res
+           makeAff \onE onS -> do
+               resBody <- unsafeCoerceEff $ newSTRef ""
+               S.onError resStream onE
+               S.onEnd resStream do
+                   b <- unsafeCoerceEff $ readSTRef resBody
+                   onS b
+               S.onDataString resStream Encoding.UTF8 \s -> unsafeCoerceEff $ void $ modifySTRef resBody (flip (<>) s)
+               pure unit
 
 makeRequest :: ∀ m. (MonadError Error m) => Webhook -> Foreign -> m Request
 makeRequest hook view = do
